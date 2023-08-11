a57f3ffad74c7be953c92315db720f38a4d7ad9d
Kevin McCarthy <km.mccarthy11@gmail.com>
2023-08-10 20:55:17 -0400

    symbology by field and popups added

--- ivf_map/src/Map.js
+++ ivf_map/src/Map.js
@@ -15,6 +15,63 @@ function Map() {
             const webmap = new WebMap({
                 basemap:'topo-vector'
             })
+
+            const renderer={
+                type: "simple",
+                field: "arrest_charge",
+                symbol: {
+                    type:"simple-marker",
+                    color: "orange",
+                    outline: {
+                        color: "white"
+                    }
+                },
+                visualVariables: [{
+                    type: "color",
+                    field: "arrest_charge",
+                    stops: [{
+                        value: "12000",
+                        color: "red"
+                    },
+                    {
+                        value: "20000",
+                        color: "orange"
+                    },
+                    {
+                        value: "26003",
+                        color: "blue"
+                    },
+                    {
+                        value: "38003",
+                        color: "yellow"
+                    },
+                    {
+                        value: "26009",
+                        color: "brown"
+                    },
+                    {
+                        value: "70000",
+                        color: "purple"
+                    },
+                    {
+                        value: "24001",
+                        color: "black"
+                    },
+                    {
+                        value: "13002",
+                        color: "green"
+                    },
+                ]
+                }]
+
+            };
+            
+            const template={
+                title: "badass motherfucker",
+                content: "Crime Category: {offense_description} at {address}" 
+            }
+
+
             view = new MapView({
                 map:webmap,
                 center:[-83.0458, 42.3314],
@@ -24,7 +81,9 @@ function Map() {
 
             })
             const geojsonLayer = new GeoJSONLayer({
-                url:"https://raw.githubusercontent.com/adarshvarma15/mygeojson/main/RMS_Crime_Incidents%20edited.geojson"
+                url:"https://raw.githubusercontent.com/adarshvarma15/mygeojson/main/RMS_Crime_Incidents%20edited.geojson",
+                renderer: renderer,
+                popupTemplate: template,
             });
             webmap.add(geojsonLayer)
         })
@@ -37,7 +96,7 @@ function Map() {
         }
     })
     return (
-        <div style={{height:800}} ref={MapEl}>
+        <div style={{height:'100vh'}} ref={MapEl}>
 
         </div>
 
